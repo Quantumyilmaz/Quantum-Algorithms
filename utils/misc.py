cb4e010e29329355fcde94c0819d4b26bfbf1059
Ege Yilmaz <a.egeyilmaz@hotmail.com>
2022-11-22 10:01:13 +0100

    Update misc.py

--- utils/misc.py
+++ utils/misc.py
@@ -63,6 +63,8 @@ def prepare_integers(n,integers,to_gate=True):
     return circ.to_gate() if to_gate else circ
 
 def get_counts(circ,qubits,**kwargs):
+    if qubits == 'all':
+        qubits = circ.qubits
     return execute_circ(circ,qubits,**kwargs).get_counts()
 
 def counts_to_integer(counts):
